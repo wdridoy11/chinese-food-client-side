1c20173953db6813a006ca293153c1a346b30b32
Ridoy Sharif <developersridoy@gmail.com>
2023-05-01 21:02:51 +0600

    Registration page design complete

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { FaFacebook } from 'react-icons/fa';
-import { AuthContext } from '../../context/AuthProvider';
+import { FaGithub } from 'react-icons/fa';
 const googleIcon=`https://cdn-icons-png.flaticon.com/512/281/281764.png`;
 
 const Login = () => {
 
-
+    const handleLogin=(event)=>{
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+    }
 
   return (
     <div className='w-full items-center pt-32'>
@@ -14,7 +18,7 @@ const Login = () => {
             <div>
                 <div className='border border-[#ddd] p-12 md:w-1/2 mx-auto rounded-lg'>
                     <h3 className='text-2xl font-bold mb-5'>Login</h3>
-                    <form>
+                    <form onSubmit={handleLogin}>
                         <input className='w-full border-b border-[#ddd] mb-5 pb-2 text-base' type="email" name="email" id="email" placeholder='Enter your email' required/>
                         <input className='w-full border-b border-[#ddd] mb-5 pb-2 text-base' type="password" name="password" id="password" placeholder='Enter your password' required/>
                         <div className='flex justify-between'>
@@ -30,8 +34,8 @@ const Login = () => {
                 </div>
                 <div className='md:w-1/2 lg:w-1/3 mx-auto mt-6'>
                     <div className='flex items-center justify-center border border-[#ddd] py-3 px-2 rounded-full mb-3 relative cursor-pointer'>
-                        <p className='absolute top-1.5 left-2 text-4xl text-[#1877F2]'><FaFacebook></FaFacebook></p>
-                        <p className='text-base font-medium text-black'>Continue with Facebook</p>
+                        <p className='absolute top-1.5 left-2 text-4xl '><FaGithub></FaGithub></p>
+                        <p className='text-base font-medium text-black'>Continue with Github</p>
                     </div>
                     <div className='flex items-center justify-center border border-[#ddd] py-3 px-2 rounded-full mb-3 relative cursor-pointer'>
                         <p className='absolute top-1.5 left-2 text-4xl text-[#1877F2]'><img className='w-9' src={googleIcon} alt="" /></p>
--- src/pages/registration/Registration.jsx
+++ src/pages/registration/Registration.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { FaGithub } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import { AuthContext } from '../../context/AuthProvider';
+const googleIcon=`https://cdn-icons-png.flaticon.com/512/281/281764.png`;
 
 const Registration = () => {
+
+  const handleRegistration =(event)=>{
+    event.preventDefault();
+    const form = event.target;
+    const name =  form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+  }
+
   return (
-    <div>Registration</div>
+    <div className='w-full items-center pt-12'>
+    <div className='container mx-auto'>
+        <div>
+            <div className='border border-[#ddd] p-12 md:w-1/2 mx-auto rounded-lg'>
+                <h3 className='text-2xl font-bold mb-5'>Create an account</h3>
+                <form onSubmit={handleRegistration}>
+                    <input className='w-full border-b border-[#ddd] mb-5 pb-2 text-base outline-none' type="text" name="name" id="name" placeholder='Enter your name' required/>
+                    <input className='w-full border-b border-[#ddd] mb-5 pb-2 text-base outline-none' type="email" name="email" id="email" placeholder='Enter your email' required/>
+                    <input className='w-full border-b border-[#ddd] mb-5 pb-2 text-base outline-none' type="password" name="password" id="password" placeholder='password' required/>
+                    <p className='text-red-600'></p>
+                    <button className='btn text-white bg-[#F9A51A] w-full border-0 rounded-none mt-10 mb-4'>Create an account</button>
+                    <p className='text-center text-base font-medium'>Already have an account? <Link to={`/login`} className='text-[#F9A51A]'>Login</Link></p>
+                </form>
+            </div>
+            <div className='md:w-1/2 lg:w-1/3 mx-auto mt-6'>
+            <div className='flex items-center justify-center border border-[#ddd] py-3 px-2 rounded-full mb-3 relative cursor-pointer'>
+                        <p className='absolute top-1.5 left-2 text-4xl '><FaGithub></FaGithub></p>
+                        <p className='text-base font-medium text-black'>Continue with Github</p>
+                    </div>
+                <div className='flex items-center justify-center border border-[#ddd] py-3 px-2 rounded-full mb-3 relative cursor-pointer'>
+                    <p className='absolute top-1.5 left-2 text-4xl text-[#1877F2]'><img className='w-9' src={googleIcon} alt="" /></p>
+                    <p className='text-base font-medium text-black'>Continue with Google</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
   )
 }
 
