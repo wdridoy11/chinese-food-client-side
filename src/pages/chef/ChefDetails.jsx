0c9bcb98d85b22eed071d1e3e803713b1fbbdcb3
Ridoy Sharif <developersridoy@gmail.com>
2023-05-02 19:51:50 +0600

    chef details page banner added

--- src/pages/chef/ChefDetails.jsx
+++ src/pages/chef/ChefDetails.jsx
@@ -3,6 +3,7 @@ import { FaHeart } from 'react-icons/fa';
 import '@smastrom/react-rating/style.css'
 import {useLoaderData } from 'react-router-dom'
 import { Rating } from '@smastrom/react-rating';
+const bannerImg=`https://img.freepik.com/free-photo/front-view-burger-with-french-fries_23-2148234991.jpg?w=1380&t=st=1683035016~exp=1683035616~hmac=b12d6be7dcf98fc94494bacbf1888c910c991503abf46f775f161a0f6a22b7d6`;
 
 const ChefDetails = () => {
   const singleChefDetails = useLoaderData();
@@ -11,13 +12,21 @@ const ChefDetails = () => {
 
   return (
     <div>
+      <div>
+          <div className="hero h-[400px]" style={{ backgroundImage: `url(${bannerImg})` }}>
+            <div className="hero-overlay bg-opacity-50"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <h1 className="mb-5 text-5xl font-semibold">Chef Recipes Details</h1>
+            </div>
+          </div>
+      </div>
       <div className='container mx-auto'>
-          <div>
+          <div className='w-1/2 mx-auto shadow-lg mt-10 mb-20'>
               <figure>
-                  <img className='w-full h-[700px] object-cover object-top' src={img} alt="chef" />
+                  <img className='w-full rounded-t-lg' src={img} alt="chef" />
               </figure>
-              <div className='p-3 pb-5'>
-                <h2 className="card-title">Name: {name}</h2>
+              <div className='p-5'>
+                <h2 className="text-2xl font-semibold mb-2">Name: {name}</h2>
                 <p className='mb-2 text-base font-medium mt-1'>Description: {description}.</p>
                 <p className='font-semibold text-base'>Like : {like}</p>
                 <p className='mb-2 text-base font-medium mt-1'>Recipes: {recipes}</p>
@@ -26,9 +35,9 @@ const ChefDetails = () => {
           </div>
           <div>
               <h1 className="text-4xl text-center font-semibold mb-7">Recipes</h1>
-              <div className='grid grid-cols-3 gap-5 py-20'>
+              <div className='grid grid-cols-3 gap-5 pb-20'>
                   {recipesDetails && recipesDetails.map((details,index)=><div key={index} className='shadow-lg'>
-                      <img src={details.img} alt={details.recipesName} />
+                      <img className='w-full h-[350px] object-cover' src={details.img} alt={details.recipesName} />
                       <div className='py-5 px-4'>
                         <h3 className='text-2xl font-semibold capitalize'>Recipes Name : {details.recipesName}</h3>
                         <h4 className='text-xl font-semibold text-black mt-3'>Ingredients</h4>
@@ -40,7 +49,7 @@ const ChefDetails = () => {
                           {details?.cookingMethod?.map((cookingMethod,index)=><li key={index} className='text-base font-medium list-decimal'>{cookingMethod}</li>)}
                         </div>
                         <div className='flex justify-between items-center mt-3'>
-                            <p className='text-base font-medium list-decimal flex gap-1'><Rating style={{ maxWidth: 100 }} value={userRating} onChange={setUserRating} readOnly/>({rating}) </p>
+                            <p className='text-base font-medium list-decimal flex gap-1'><Rating style={{ maxWidth: 100 }} value={userRating} onChange={setUserRating} readOnly/>({details.rating}) </p>
                             <p className='text-red-500 border border-[#F9A51A] p-2 rounded-md cursor-pointer hover:bg-black duration-500'><FaHeart></FaHeart></p>
                         </div>
                       </div>
