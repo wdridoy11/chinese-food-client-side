3b45d1939ce90482ae56efd09fdc98117d8029f6
Ridoy Sharif <developersridoy@gmail.com>
2023-05-02 12:28:14 +0600

    chef card design

---
+++ src/pages/chef/AllChefShow.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const AllChefShow = (props) => {
+  const {id,img,like,name,rating,experience}=props.singleChef;
+  console.log(props.singleChef)
+  return (
+    <div>
+      <div>
+        <div className="card bg-base-100 shadow-xl">
+          <figure>
+              <img className='w-full h-[300px] object-cover object-top' src={img} alt="chef" />
+          </figure>
+          <div className='p-3 pb-5'>
+            <h2 className="card-title">Name: {name}</h2>
+            <p className='mb-5 text-base font-medium mt-1'>Experience: {experience} years experience</p>
+            <Link className='bg-[#F9A51A] px-5 py-2 rounded-md text-base font-semibold text-black hover:bg-black hover:text-white duration-500'>View Recipes</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AllChefShow
--- src/pages/chef/ChefDataLoad.jsx
+++ src/pages/chef/ChefDataLoad.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { useLoaderData } from 'react-router-dom'
+import AllChefShow from './AllChefShow';
 
 const ChefDataLoad = () => {
     const chefData = useLoaderData();
-    console.log(chefData)
   return (
     <div>
-
+        <div className='container mx-auto'>
+            <div className='grid grid-cols-3 gap-5'>
+              {chefData.map((singleChef)=><AllChefShow key={singleChef.id} singleChef={singleChef}></AllChefShow>)}
+            </div>
+        </div>
     </div>
   )
 }
